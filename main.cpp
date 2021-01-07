f424e97ecce8e8883a907df81bf6b18da0a8aafc
jkolanowski <jkolanowski@atlassian.com>
2021-01-07 13:23:05 +0100

    ioctl added

--- main.cpp
+++ main.cpp
@@ -1,19 +1,22 @@
 #include <iostream>
 #include <unistd.h>
 #include <sys/socket.h>
+#include <sys/ioctl.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <cstring>
 #include <sstream>
+#include <map>
 
 #define SERVER_PORT 1234
 #define QUEUE_SIZE 5
-#define BUFFER_LENGTH 100
+#define BUFFER_LENGTH 240
 
 int main(int argc, char *argv[]) {
     int nSocket, nClientSocket;
     int nBind, nListen;
     int nFoo = 1;
+    std::map<std::string, std::string> notes;
     socklen_t nTmp;
     sockaddr_in stAddr{}, stClientAddr{};
 
@@ -54,17 +57,24 @@ int main(int argc, char *argv[]) {
 
         printf("%s: [connection from %s]\n", argv[0], inet_ntoa((in_addr) stClientAddr.sin_addr));
 
-        //toDo do something with read!!!
-        char buffer[BUFFER_LENGTH];
+        //toDo do something with read!!! Maybe done
+        int len = 0;
+        do {
+            ioctl(nClientSocket, FIONREAD, &len);
+        } while (len < 1);
+
+        char buffer[len];
         std::string data = "";
         int f = 0;
-        do {
-            f = read(nClientSocket, buffer, BUFFER_LENGTH);
-            data.append(buffer);
-            memset(buffer, 0, BUFFER_LENGTH);
-        } while (f == BUFFER_LENGTH);
+        f = read(nClientSocket, buffer, len);
+        data.append(buffer);
+        memset(buffer, 0, len);
 
-        std::cout << data;
+//        do {
+
+//        } while (f == BUFFER_LENGTH);
+
+        std::cout << data << "\n\n";
 
         std::string msg;
 
@@ -99,8 +109,7 @@ int main(int argc, char *argv[]) {
             msg = "HTTP/1.1 200 OK\n"
                   "Content-Length: 0\n"
                   "Content-Type: application/json";
-        }
-        else { //TODO FIX BAD REQUEST
+        } else { //TODO FIX BAD REQUEST
             msg = "HTTP/1.1 400 Bad Request\n"
                   "Content-Length: 100\n"
                   "Content-Type: text/html\n";
