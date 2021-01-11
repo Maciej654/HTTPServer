83a364ec42e7bc5a4a458dabea6ccff868d823c8
jkolanowski <jkolanowski@atlassian.com>
2021-01-12 00:50:46 +0100

    Added terminal arguments handling.

--- files/main.cpp
+++ files/main.cpp
@@ -1,7 +1,22 @@
 #include "Server.h"
 
 int main(int argc, char *argv[]) {
-    Server server(1234);
-    server.runServer();
+    if(argc > 2){
+        printf("Too many arguments supplied. Exiting.");
+        return -1;
+    }else if (argc == 2) {
+        char *ptr;
+        int port;
+        port = (int) strtol(argv[1], &ptr, 10);
+        if(port > 65353 || port < 0) {
+            printf("Wrong port has been chosen. Exiting.");
+            return -1;
+        }
+        Server server(port);
+        server.runServer();
+    } else {
+        Server server (80);
+        server.runServer();
+    }
 }
 
